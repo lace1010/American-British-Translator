a8f2fd1bbbc4143388b3617c05bd381d2dd5473b
Hunter Lacefield <lace1010@gmail.com>
2021-03-24 14:46:45 +0800

    Finished translating titles and timeboth ways

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,61 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  americanToBritish(text) {
+    // First split the string into array of all words.
+    let splitTextArray = text.split(" ");
+    let timeRegex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]/;
 
+    // Check every word and see if a time is there. If so then return the british form of time.
+    let updatedTimeArray = splitTextArray.map((i) => {
+      if (timeRegex.test(i)) {
+        return (i = "<span class='highliht'>" + i.replace(":", ".") + "<span>");
+      } else return i;
+    });
+
+    // loop through each word and if one matches a property in title object change it to british one
+    let updatedTitlesArray = updatedTimeArray.map((i) => {
+      if (americanToBritishTitles.hasOwnProperty(i)) {
+        return (
+          (i = "<span class='highliht'>" + americanToBritishTitles[i]) +
+          "<span>"
+        );
+      } else return i;
+    });
+
+    let updatedString = updatedTitlesArray.join(" ");
+    console.log(updatedString, "<=updatedString for american to british");
+    return { text: text, translation: updatedString };
+  }
+
+  britishToAmerican(text) {
+    let timeRegex = /^([0-1]?[0-9]|2[0-3])\.[0-5][0-9]/;
+    let splitTextArray = text.split(" ");
+    let updatedTimeArray = splitTextArray.map((i) => {
+      if (timeRegex.test(i)) {
+        return (i = "<span class='highliht'>" + i.replace(".", ":") + "<span>");
+      } else return i;
+    });
+
+    let updatedTitlesArray = updatedTimeArray.map((i) => {
+      if (Object.values(americanToBritishTitles).includes(i)) {
+        // This is a way to return the key based on found value
+        let key = Object.keys(americanToBritishTitles)[
+          Object.values(americanToBritishTitles).indexOf(i)
+        ];
+        // Now return key that is based off of the i found
+        return (i = "<span class='highliht'>" + key + "<span>");
+      } else return i;
+    });
+
+    let updatedString = updatedTitlesArray.join(" ");
+    console.log(updatedString, "<=updatedString for British to American");
+
+    return { text: text, translation: updatedString };
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,49 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    let text = req.body.text;
+    let locale = req.body.locale;
+    let updatedString = "";
+
+    // If one or more of the required fields is missing
+    if (!text || !locale) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+    // If text is empty
+    else if (text == "") return res.json({ error: "No text to translate" });
+    // If locale does not match one of the two specified locales
+    else if (
+      locale !== "american-to-british" &&
+      locale !== "british-to-american"
+    ) {
+      return res.json({ error: "Invalid value for locale field" });
+    }
+    // If locale is american to british
+    else if (locale == "american-to-british") {
+      updatedString = translator.americanToBritish(text).translation;
+      // If text requires no translation (If text and translated string are the same)
+      if (text == updatedString) {
+        return res.json({ translation: "Everything looks good to me!" });
+      }
+      // else return translated object (updatedString is inside object)
+      else return res.json(translator.americanToBritish(text));
+    }
+    // If locale is british to american
+    else if (locale === "british-to-american") {
+      updatedString = translator.britishToAmerican(text).translation;
+      console.log(updatedString, "<= updated string in api.js");
+      console.log(text, "<= text in api.js");
+      // If text requires no translation (If text and translated string are the same)
+      if (text == updatedString) {
+        return res.json({ translation: "Everything looks good to me!" });
+      }
+      // else return translated object (updatedString is inside object)
+      return res.json(translator.britishToAmerican(text));
+    }
+  });
 };
