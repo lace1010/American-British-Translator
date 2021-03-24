2bc48a401dc6b81fc40747a10f82c6931f9c6758
Hunter Lacefield <lace1010@gmail.com>
2021-03-24 15:10:44 +0800

    Fixed error logic to pass tests

--- routes/api.js
+++ routes/api.js
@@ -10,46 +10,47 @@ module.exports = function (app) {
     let locale = req.body.locale;
     let updatedString = "";
 
-    // If one or more of the required fields is missing
-    if (!text || !locale) {
-      return res.json({ error: "Required field(s) missing" });
-    }
-    // If text is empty
-    else if (!text) return res.json({ error: "No text to translate" });
-    // If locale does not match one of the two specified locales
-    else if (
-      locale !== "american-to-british" &&
-      locale !== "british-to-american"
-    ) {
-      return res.json({ error: "Invalid value for locale field" });
-    }
-    // If locale is american to british
-    else if (locale == "american-to-british") {
-      updatedString = translator.americanToBritish(text).translation;
-      // If text requires no translation (If text and translated string are the same)
-      if (text == updatedString) {
-        return res.json({
-          text: text,
-          translation: "Everything looks good to me!",
-        });
+    // If both fields exist
+    if (text && locale) {
+      // If text is empty
+      if (!text) return res.json({ error: "No text to translate" });
+      // If locale does not match one of the two specified locales
+      else if (
+        locale !== "american-to-british" &&
+        locale !== "british-to-american"
+      ) {
+        return res.json({ error: "Invalid value for locale field" });
       }
-      // else return translated object (updatedString is inside object)
-      else return res.json(translator.americanToBritish(text));
-    }
-    // If locale is british to american
-    else if (locale === "british-to-american") {
-      updatedString = translator.britishToAmerican(text).translation;
-      console.log(updatedString, "<= updated string in api.js");
-      console.log(text, "<= text in api.js");
-      // If text requires no translation (If text and translated string are the same)
-      if (text == updatedString) {
-        return res.json({
-          text: text,
-          translation: "Everything looks good to me!",
-        });
+      // If locale is american to british
+      else if (locale == "american-to-british") {
+        updatedString = translator.americanToBritish(text).translation;
+        // If text requires no translation (If text and translated string are the same)
+        if (text == updatedString) {
+          return res.json({
+            text: text,
+            translation: "Everything looks good to me!",
+          });
+        }
+        // else return translated object (updatedString is inside object)
+        else return res.json(translator.americanToBritish(text));
       }
-      // else return translated object (updatedString is inside object)
-      return res.json(translator.britishToAmerican(text));
+      // If locale is british to american
+      else if (locale === "british-to-american") {
+        updatedString = translator.britishToAmerican(text).translation;
+        console.log(updatedString, "<= updated string in api.js");
+        console.log(text, "<= text in api.js");
+        // If text requires no translation (If text and translated string are the same)
+        if (text == updatedString) {
+          return res.json({
+            text: text,
+            translation: "Everything looks good to me!",
+          });
+        }
+        // else return translated object (updatedString is inside object)
+        return res.json(translator.britishToAmerican(text));
+      }
     }
+    // else if both fields do not exist
+    else return res.json({ error: "Required field(s) missing" });
   });
 };
