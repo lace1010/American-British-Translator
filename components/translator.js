382003f8f281952a26326f19a817394b82adb62c
Hunter Lacefield <lace1010@gmail.com>
2021-03-25 14:29:11 +0800

    Translate from american to british and vice versa with correct class added for a green highlight

--- components/translator.js
+++ components/translator.js
@@ -7,23 +7,33 @@ class Translator {
   americanToBritish(text) {
     let updatedString = text;
 
-    // Loops through every object key in americanToBritishTitles and checks if it is in the text. If so replace i with the british value.
-    Object.keys(americanToBritishTitles).map((i) => {
-      if (updatedString.includes(i)) {
-        updatedString = text.replace(i, americanToBritishTitles[i]);
-      }
-    });
     // Loops throught every object key in americanOnly and checks if it is in the text. If so replace i with the british value.
     Object.keys(americanOnly).map((i) => {
       if (updatedString.includes(i)) {
-        updatedString = updatedString.replace(i, americanOnly[i]);
+        updatedString = updatedString.replace(
+          i,
+          '<span class="highlight">' + americanOnly[i] + "</span>"
+        );
       }
     });
 
     // Loops throught every object key in americanToBritishSpelling and checks if it is in the text. If so replace i with the british value.
     Object.keys(americanToBritishSpelling).map((i) => {
       if (updatedString.includes(i)) {
-        updatedString = updatedString.replace(i, americanToBritishSpelling[i]);
+        updatedString = updatedString.replace(
+          i,
+          '<span class="highlight">' + americanToBritishSpelling[i] + "</span>"
+        );
+      }
+    });
+
+    // Loops through every object key in americanToBritishTitles and checks if it is in the text. If so replace i with the british value.
+    Object.keys(americanToBritishTitles).map((i) => {
+      if (updatedString.includes(i)) {
+        updatedString = updatedString.replace(
+          i,
+          '<span class="highlight">' + americanToBritishTitles[i] + "</span>"
+        );
       }
     });
 
@@ -36,41 +46,80 @@ class Translator {
     let updatedArray = splitTextArray.map((i) => {
       // If one of the word slots has an american time replace it with British time
       if (timeRegex.test(i)) {
-        return (i = i.replace(":", "."));
+        return (i =
+          '<span class="highlight">' + i.replace(":", ".") + "</span>");
       } else return i;
     });
 
     // rejoin the updatedArray to have updatedString now translate to correct british time format.
     updatedString = updatedArray.join(" ");
 
-    console.log(updatedString, "<=updatedString for american to british");
     return { text: text, translation: updatedString };
   }
 
   // Fix britishToAmerican function just like americanToBritish
 
   britishToAmerican(text) {
-    // Figure out a way to split array with special characters and full stops but not one in time regex
-    let splitTextArray = text.split(" ");
+    let updatedString = text;
+    let key;
+
+    // For britishOnly the key is British and value is American so we loop through Object.keys because we are translating to American
+    Object.keys(britishOnly).map((i) => {
+      if (updatedString.includes(i)) {
+        updatedString = updatedString.replace(
+          i,
+          '<span class="highlight">' + britishOnly[i] + "</span>"
+        );
+      }
+    });
+
+    //  Loops through every object value in americanToBritishSpelling and checks if it is in the text. If so replace i with the american key.
+    Object.values(americanToBritishSpelling).map((i) => {
+      //  If string includes a value in americanToBritishTitles object
+      if (updatedString.includes(i)) {
+        // This is a way to return the key based on found value
+        key = Object.keys(americanToBritishSpelling)[
+          Object.values(americanToBritishSpelling).indexOf(i)
+        ];
+        // Now return key that is bassed off the i (value) found
+        updatedString = updatedString.replace(
+          i,
+          '<span class="highlight">' + key + "</span>"
+        );
+      }
+    });
+
+    //  Loops through every object value in americanToBritishTitles and checks if it is in the text. If so replace i with the american key.
+    Object.values(americanToBritishTitles).map((i) => {
+      //  If string includes a value in americanToBritishTitles object
+      if (updatedString.includes(i)) {
+        // This is a way to return the key based on found value
+        key = Object.keys(americanToBritishTitles)[
+          Object.values(americanToBritishTitles).indexOf(i)
+        ];
+
+        // if updatedString does not involve the american version with . after then update string
+        if (!updatedString.includes(key)) {
+          // Now return key that is bassed off the i (value) found
+          updatedString = updatedString.replace(
+            i,
+            '<span class="highlight">' + key + "</span>"
+          );
+        }
+      }
+    });
+
+    // split updatedString to translate times now
+    let splitTextArray = updatedString.split(" ");
     let timeRegex = /^([0-1]?[0-9]|2[0-3])\.[0-5][0-9]/;
     let updatedArray = splitTextArray.map((i) => {
       // If one of the word slots has a british time
       if (timeRegex.test(i)) {
-        return (i = i.replace(".", ":"));
-      }
-      // If i is one of the values in object americanToBritishTitles
-      else if (Object.values(americanToBritishTitles).includes(i)) {
-        // This is a way to return the key based on found value
-        let key = Object.keys(americanToBritishTitles)[
-          Object.values(americanToBritishTitles).indexOf(i)
-        ];
-        // Now return key that is based off of the i found
-        return (i = key);
+        return (i =
+          '<span class="highlight">' + i.replace(".", ":") + "</span>");
       } else return i;
     });
-
-    let updatedString = updatedArray.join(" ");
-    console.log(updatedString, "<=updatedString for British to American");
+    updatedString = updatedArray.join(" ");
 
     return { text: text, translation: updatedString };
   }
